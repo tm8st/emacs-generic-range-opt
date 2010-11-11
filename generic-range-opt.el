e6b157ed1bc076f01d2b4a3a380bc1d893c49a6a
tm8st <tm8st@hotmail.co.jp>
2010-11-12 03:01:51 +0900

    refactoring base-function.

--- generic-range-opt.el
+++ generic-range-opt.el
@@ -67,98 +67,54 @@
 (eval-when-compile (require 'cl))
 (require 'thing-opt)
 
-(defun generic-range-opt-delete-base-function (begin-move-func &optional end-move-func &optional mark-func)
+(defun generic-range-opt-base-function (range-opt-runction begin-move-func &optional end-move-func &optional mark-func)
   (interactive)
   (save-excursion
     (if mark-active
-	(delete-region (point) (mark))
-      (let* ((start-point (point)) (s (progn (when begin-move-func
-			(funcall begin-move-func))
-		      (point))))
-	(let ((e
-	       (progn (when end-move-func
-			(goto-char start-point)
-			(funcall end-move-func))
-		      (point))))
-	  (when mark-func
-	    (goto-char start-point)
-	    (funcall mark-func)
-	    (setq e (point))
-	    (setq s (mark)))
-	  (delete-region s e))))))
-
-(defun generic-range-opt-kill-base-function (begin-move-func &optional end-move-func &optional mark-func)
-  (interactive)
-  (save-excursion
-    (if mark-active
-	(kill-region (point) (mark))
-    (let* ((start-point (point)) (s (progn (when begin-move-func
-			(funcall begin-move-func))
-		      (point))))
-	(let ((e
-	       (progn (when end-move-func
-			(goto-char start-point)
-			(funcall end-move-func))
-		      (point))))
+	(funcall range-opt-runction (point) (mark))
+      (let* ((start-point (point))
+	     (s (cond
+		 (begin-move-func (funcall begin-move-func) (point))
+		 (t start-point)))
+	     (e (cond
+		 (end-move-func (goto-char start-point) (funcall end-move-func) (point))
+		 (t start-point))))
 	(when mark-func
 	  (goto-char start-point)
 	  (funcall mark-func)
-	  (setq s (mark))
-	  (setq e (point)))
-	(kill-region s e)
-	)))))
-
-(defun generic-range-opt-copy-base-function (begin-move-func &optional end-move-func &optional mark-func)
-  (interactive)
-  (save-excursion
-    (if mark-active
-	(copy-region-as-kill (point) (mark))
-      (let* ((start-point (point)) (s (progn (when begin-move-func
-			(funcall begin-move-func))
-		      (point))))
-	(let ((e
-	       (progn (when end-move-func
-			(goto-char start-point)
-			(funcall end-move-func))
-		      (point))))
-	  (when mark-func
-	    (goto-char start-point)
-	    (funcall mark-func)
-	    (setq s (mark))
-	    (setq e (point)))
-	  (copy-region-as-kill s e)
-	  )))))
+	  (setq e (point))
+	  (setq s (mark)))
+	(funcall range-opt-runction s e)))))
 
 (defun generic-range-opt-mark-base-function (begin-move-func &optional end-move-func &optional mark-func)
   (interactive)
-  (let ((start-point (point))(s (point)) (e (point)))
-    (save-excursion
-      (setq s (progn (when begin-move-func
-		       (funcall begin-move-func))
-		     (point)))
-      (setq e (progn (when end-move-func
-		       (goto-char start-point)
-		       (funcall end-move-func))
-		     (point)))
-      (when mark-func
-	(goto-char start-point)
-	(funcall mark-func)
-	(setq s (mark))
-	(setq e (point))))
-    (goto-char s)
-    (push-mark e nil transient-mark-mode)
-    ))
+  (let* ((start-point (point))
+	 (s (cond
+	     (begin-move-func (funcall begin-move-func) (point))
+	     (t start-point)))
+	 (e (cond
+	     (end-move-func (goto-char start-point) (funcall end-move-func) (point))
+	     (t start-point))))
+    (when mark-func
+      (goto-char start-point)
+      (funcall mark-func)
+      (setq e (point))
+      (setq s (mark)))
+    (push-mark e t transient-mark-mode)
+    ;; (push-mark s t transient-mark-mode)
+    ;; ))
+    (goto-char s)))
 
 (defmacro generic-range-opt (name begin end &optional mark)
   "generate range operations. mark, delete, kill"
   (fset (intern (concat "gro-mark-" name))
 	`(lambda () (interactive) (generic-range-opt-mark-base-function ,begin ,end ,mark)))
   (fset (intern (concat "gro-delete-" name))
-	`(lambda () (interactive) (generic-range-opt-delete-base-function ,begin ,end ,mark)))
+	`(lambda () (interactive) (generic-range-opt-base-function 'delete-region ,begin ,end ,mark)))
   (fset (intern (concat "gro-copy-" name))
-	`(lambda () (interactive) (generic-range-opt-copy-base-function ,begin ,end ,mark)))
+	`(lambda () (interactive) (generic-range-opt-base-function 'copy-region-as-kill ,begin ,end ,mark)))
   (fset (intern (concat "gro-kill-" name))
-	`(lambda () (interactive) (generic-range-opt-kill-base-function ,begin ,end ,mark)))
+	`(lambda () (interactive) (generic-range-opt-base-function 'kill-region ,begin ,end ,mark)))
   )
 
 ;; generate range operation.
@@ -168,7 +124,7 @@
 (generic-range-opt "backward-line" nil 'beginning-of-line)
 (generic-range-opt "search-forward-char" nil
 		   '(lambda () (re-search-forward (char-to-string (read-char "search forward:")))))
-(generic-range-opt "search-bacckward-char" nil
+(generic-range-opt "search-backward-char" nil
 		   '(lambda () (re-search-backward (char-to-string (read-char "search backward:")))))
 (generic-range-opt "follow-word" nil nil 'mark-word*)
 (generic-range-opt "follow-sexp" nil nil 'mark-sexp*)
@@ -177,6 +133,14 @@
 (generic-range-opt "defun*" nil nil 'mark-defun*)
 (generic-range-opt "prev-line" nil nil '(lambda () (previous-line) (mark-line)))
 (generic-range-opt "next-line" nil nil '(lambda () (next-line) (mark-line)))
-(generic-range-opt "jaunte-prev" nil nil '(lambda () (jaunte) (backward-char)))
+(generic-range-opt "goto-line" nil '(lambda () (goto-line (read-number "line number?:")) (end-of-line)))
+
+(when (require 'jaunte nil t)
+  (generic-range-opt "jaunte-prev" nil nil '(lambda () (jaunte) (backward-char)))
+  (generic-range-opt "between-jaunte" 'jaunte 'jaunte))
+
+(when (require 'yafastnav nil t)
+  (generic-range-opt "yafastnav-prev" nil nil '(lambda () (yafastnav-jump-to-forward) (backward-char)))
+  (generic-range-opt "between-yafastnav" 'yafastnav-jump-to-backward 'yafastnav-jump-to-forward))
 
 (provide 'generic-range-opt)
